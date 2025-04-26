61974ce5582d3991f88298b770251cfafc985ddf
Omar Barouni <omar_barouni@hotmail.com>
2025-04-26 01:53:03 +0100

    Use remote S3 backend for Terraform state and set AWS region

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,21 @@
 
+terraform {
+   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "gdpr-obfuscator-terraform-state"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
 
 provider "aws" {
   region = "eu-west-2"
 }
-
-
 
 
 
