d12069c1c2be8a19e00d6bf78db2e5a93d36f890
Omar Barouni <omar_barouni@hotmail.com>
2025-04-27 02:20:13 +0100

    Inject SNS email via GitHub Secrets and Terraform variable for secure configuration

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -5,5 +5,5 @@ resource "aws_sns_topic" "obfuscation_alerts" {
 resource "aws_sns_topic_subscription" "email_alert" {
   topic_arn = aws_sns_topic.obfuscation_alerts.arn
   protocol  = "email"
-  endpoint  = "omar_barouni@hotmail.com"  
+  endpoint  = var.SNS_EMAIL 
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
 variable "bucket_name" {
     default = "gdpr-obfuscator-raw"
 }
+
+variable "SNS_EMAIL" {
+    type = string
+  
+}
